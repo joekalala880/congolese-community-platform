70579cce04cada643acfc91548f63aed3394ca1a
kalalajoel <kapingad04@gmail.com>
2026-05-27 23:00:25 -0400

    Fix Render deployment

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const connectDB = require("./config/db");
+
 const authRoutes = require("./routes/authRoutes");
 const housingRoutes = require("./routes/housingRoutes");
 const immigrationRoutes = require("./routes/immigrationRoutes");
@@ -12,22 +13,23 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-
 connectDB();
-app.use("/api/healthcare", healthcareRoutes);
-app.use("/api/food", foodRoutes);
+
 app.use("/api/auth", authRoutes);
 app.use("/api/housing", housingRoutes);
 app.use("/api/immigration", immigrationRoutes);
-app.get("/test-housing", (req, res) => {
-  res.send("Housing route test works");
-});
+app.use("/api/food", foodRoutes);
+app.use("/api/healthcare", healthcareRoutes);
 
 app.get("/", (req, res) => {
   res.send("Welcome to Congolese Community Support System");
 });
 
-const PORT = 3001;
+app.get("/test-housing", (req, res) => {
+  res.send("Housing route test works");
+});
+
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
