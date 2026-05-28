63da1f56f73d47a4029de9610cf0b08964974a17
kalalajoel <kapingad04@gmail.com>
2026-05-28 19:29:16 -0400

    Use MongoDB Atlas URI from environment

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,14 +1,14 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-    try {
-        await mongoose.connect("mongodb://127.0.0.1:27017/congoleseCommunity");
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
 
-        console.log("MongoDB Connected");
-    } catch (error) {
-        console.error("Database connection failed");
-        process.exit(1);
-    }
+    console.log("MongoDB Connected");
+  } catch (error) {
+    console.error("Database connection failed:", error.message);
+    process.exit(1);
+  }
 };
 
 module.exports = connectDB;
