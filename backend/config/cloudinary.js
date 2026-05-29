c26d63071a23de552cadbf540bd3efa2941c3b28
kalalajoel <kapingad04@gmail.com>
2026-05-29 19:56:42 -0400

    debug cloudinary

--- backend/config/cloudinary.js
+++ backend/config/cloudinary.js
@@ -1,9 +1,13 @@
 const cloudinary = require("cloudinary").v2;
 
+console.log("Cloud Name:", process.env.CLOUDINARY_CLOUD_NAME);
+console.log("API Key:", process.env.CLOUDINARY_API_KEY);
+console.log("Secret Exists:", !!process.env.CLOUDINARY_API_SECRET);
+
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-module.exports = cloudinary;
+module.exports = cloudinary
