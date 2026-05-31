da59aa4ce2ccf1681205b8840c5623b551fe5720
kalalajoel <kapingad04@gmail.com>
2026-05-31 13:47:51 -0400

    Hide user passwords from users API

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 router.get("/", async (req, res) => {
   try {
-    const users = await User.find().sort({ createdAt: -1 });
+  const users = await User.find().select("-password").sort({ createdAt: -1 });
     res.status(200).json(users);
   } catch (error) {
     res.status(500).json({ message: error.message });
