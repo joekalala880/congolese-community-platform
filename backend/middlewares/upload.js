baa7cff914f3a443e257ac91bef7f7aaf3ab0699
kalalajoel <kapingad04@gmail.com>
2026-06-10 17:34:33 -0400

    Finalize housing upload feature

--- backend/middlewares/upload.js
+++ backend/middlewares/upload.js
@@ -7,17 +7,27 @@ const storage = new CloudinaryStorage({
   params: {
     folder: "congolese-community-uploads",
     resource_type: "auto",
-    allowed_formats: ["jpg", "jpeg", "png", "webp", "pdf"],
+   allowed_formats: [
+  "jpg",
+  "jpeg",
+  "png",
+  "webp",
+  "pdf",
+  "doc",
+  "docx",
+],
   },
 });
 
 const fileFilter = (req, file, cb) => {
-  const allowedTypes = [
-    "image/jpeg",
-    "image/png",
-    "image/webp",
-    "application/pdf",
-  ];
+ const allowedTypes = [
+  "image/jpeg",
+  "image/png",
+  "image/webp",
+  "application/pdf",
+  "application/msword",
+  "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+];
 
   if (allowedTypes.includes(file.mimetype)) {
     cb(null, true);
